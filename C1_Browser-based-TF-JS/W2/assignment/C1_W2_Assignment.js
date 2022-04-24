c54486b6935865491c65bf38524c11ad255a311c
andre002wp <andre002wp@gmail.com>
2022-04-24 23:12:43 +0800

    C1 week 1 and 2

--- C1_Browser-based-TF-JS/W2/assignment/C1_W2_Assignment.js
+++ C1_Browser-based-TF-JS/W2/assignment/C1_W2_Assignment.js
@@ -16,11 +16,21 @@ function getModel() {
     model = tf.sequential();
     
     // YOUR CODE HERE
-    
+    model.add(tf.layers.conv2d({kernelSize:3, filters:32, activation: "relu", inputShape:[28,28,1]}));
+    model.add(tf.layers.maxPooling2d({poolSize: [2,2]}));
+    model.add(tf.layers.conv2d({kernelSize:3, filters:16, activation: "relu"}));
+    model.add(tf.layers.maxPooling2d({poolSize: [2,2]}));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({units:256, activation: "relu"}));
+    model.add(tf.layers.dense({units:10, activation: "softmax"}));
     
     // Compile the model using the categoricalCrossentropy loss,
     // the tf.train.adam() optimizer, and `acc` for your metrics.
-    model.compile(// YOUR CODE HERE);
+    model.compile({
+        loss: "categoricalCrossentropy", 
+        optimizer: tf.train.adam(), 
+        metrics: ["acc"]
+    });
     
     return model;
 }
@@ -28,17 +38,20 @@ function getModel() {
 async function train(model, data) {
         
     // Set the following metrics for the callback: 'loss', 'val_loss', 'acc', 'val_acc'.
-    const metrics = // YOUR CODE HERE    
+    const metrics = ['loss', 'val_loss', 'acc', 'val_acc']; 
 
         
     // Create the container for the callback. Set the name to 'Model Training' and 
     // use a height of 1000px for the styles. 
-    const container = // YOUR CODE HERE   
+    const container = {
+        name:'Model Training',
+        styles:{height:'1000px'}
+    };
     
     
     // Use tfvis.show.fitCallbacks() to setup the callbacks. 
     // Use the container and metrics defined above as the parameters.
-    const fitCallbacks = // YOUR CODE HERE
+    const fitCallbacks = tfvis.show.fitCallbacks(container,metrics);
     
     const BATCH_SIZE = 512;
     const TRAIN_DATA_SIZE = 6000;
@@ -47,13 +60,25 @@ async function train(model, data) {
     // Get the training batches and resize them. Remember to put your code
     // inside a tf.tidy() clause to clean up all the intermediate tensors.
     // HINT: Take a look at the MNIST example.
-    const [trainXs, trainYs] = // YOUR CODE HERE
+    const [trainXs, trainYs] = tf.tidy(() => {
+        const b = data.nextTrainBatch(TRAIN_DATA_SIZE);
+        return [
+            b.xs.reshape([TRAIN_DATA_SIZE,28,28,1]),
+            b.labels
+        ];
+    });
 
     
     // Get the testing batches and resize them. Remember to put your code
     // inside a tf.tidy() clause to clean up all the intermediate tensors.
     // HINT: Take a look at the MNIST example.
-    const [testXs, testYs] = // YOUR CODE HERE
+    const [testXs, testYs] = tf.tidy(() => {
+        const b = data.nextTrainBatch(TEST_DATA_SIZE);
+        return [
+            b.xs.reshape([TEST_DATA_SIZE,28,28,1]),
+            b.labels
+        ];
+    });
 
     
     return model.fit(trainXs, trainYs, {
